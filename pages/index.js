657fbc6bac46bf45077527fe33eb640da39e1a00
Elena Troenko <troenko1@yandex.ru>
2023-01-14 19:00:21 +0300

    начало работы над проектом

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+// Переменные
+let profileName = document.querySelector('.profile__name');  // абзац Имя на странице
+let profileAboutYourSelf = document.querySelector('.profile__about-yourself');  // абзац О себе на странице
+let popup = document.querySelector('.popup');  // секция popup
+let popupForm = popup.querySelector('.popup-form');  // форма
+let inputName = popupForm.querySelector('.popup-form__input-name');  // поле Имя в форме
+let inputJob = popupForm.querySelector('.popup-form__input-job');  // поле О себе в форме
+
+
+// Показать форму
+function showForm(){
+  popup.classList.add('popup-show');
+
+  inputName.value = profileName.textContent;
+  inputJob.value = profileAboutYourSelf.textContent;
+}
+
+
+// Скрыть форму
+function hideForm(){
+  popup.classList.remove('popup-show');
+}
+
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();  // не перегружать страницу в браузере
+  
+  profileName.textContent = inputName.value;
+  profileAboutYourSelf.textContent = inputJob.value;
+  hideForm();
+}  
+
+
+// Добавить слушателей событий
+function addEvents() {
+  document.querySelector('.profile__btn-edit').addEventListener('click', showForm);
+  document.querySelector('.popup__btn-close').addEventListener('click', hideForm);
+  popupForm.addEventListener('submit', handleFormSubmit);
+}
+
+
+// Добавить события
+addEvents();
