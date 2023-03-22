e56ff4125145026d8dbaeabbd3de2f2be516df2e
Elena Troenko <troenko1@yandex.ru>
2023-03-22 08:23:03 +0300

    fix: изменение класса Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -3,26 +3,14 @@ class Card {
     // Селекторы
     this._selectors = selectors;
 
-    // загрузка template
-    this._loadTemplate();
-
     this._name = data.name;
     this._link = data.link;
 
     this._zoomCallBack = zoomCallBack;
-
-    // Установить свойства объекта
-    this._cardElement = this._template.querySelector(this._selectors.cardElementSelector).cloneNode(true);  // Карточка места
-    this._cardElementImg = this._cardElement.querySelector(this._selectors.cardElementImgSelector);         // Фотография места
-    this._heartElement =  this._cardElement.querySelector(this._selectors.heartElementSelector);            // Сердечко
-    this._basketBtn =  this._cardElement.querySelector(this._selectors.basketBtnSelector);                  // Корзина
-
-    // установить слушателей событий
-    this._setEventListeners();
   }
 
   _loadTemplate() {
-    this._template = document.querySelector(this._selectors.templateSelector).content;                            // Шаблон карточки места
+    this._template = document.querySelector(this._selectors.templateSelector).content;  // Шаблон карточки места
   }
 
   // Хэндлер для удаления карточки
@@ -40,19 +28,37 @@ class Card {
     this._zoomCallBack(this._name, this._cardElementImg.src);
   }
 
+  // установить слушателей событий
   _setEventListeners() {
     this._basketBtn.addEventListener('click', () => this._handleRemoveCard());    // добавить событие удаления карточки
     this._heartElement.addEventListener('click', () => this._handleChangeHeart());  // добавить событие смены сердечка    
     this._cardElementImg.addEventListener('click', () => this._handleCallZoom());  // добавить событие показа Зума
   }
+  
+  // заполняет карту данными
+  _fillCard() {
+     // Установить свойства объекта
+     this._cardElement = this._template.querySelector(this._selectors.cardElementSelector).cloneNode(true);  // Карточка места
+     this._cardElementImg = this._cardElement.querySelector(this._selectors.cardElementImgSelector);         // Фотография места
+     this._heartElement =  this._cardElement.querySelector(this._selectors.heartElementSelector);            // Сердечко
+     this._basketBtn =  this._cardElement.querySelector(this._selectors.basketBtnSelector);                  // Корзина
+ 
+     // Заполнить title, link, alt
+     this._cardElement.querySelector(this._selectors.cardElementTitleSelector).textContent = this._name;
+     this._cardElementImg.src = this._link;
+     this._cardElementImg.alt = `Фото места: ${this._name}`;
+  }
 
   // Возвращает элемент карточки
   getCard() {
-    // Заполнить title, link, alt
-    this._cardElement.querySelector(this._selectors.cardElementTitleSelector).textContent = this._name;
-    this._cardElementImg.src = this._link;
-    this._cardElementImg.alt = `Фото места: ${this._name}`;
 
+    // загрузка template
+    this._loadTemplate();
+  
+    this._fillCard();
+   
+    this._setEventListeners();
+    
     return this._cardElement;
   }
 }
